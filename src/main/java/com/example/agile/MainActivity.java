1cc7304dd3ce951f3edfb4eeb343df66552936a5
Gamepro <1997gamepro@gmail.com>
2020-01-26 12:22:13 +0800

    first commit

---
+++ src/main/java/com/example/agile/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.agile;
+
+import android.app.*;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
