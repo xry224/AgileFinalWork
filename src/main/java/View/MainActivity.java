7e497bf6ad0c03a3b34b1115a5764840bcb2a00e
Gamepro <1997gamepro@gmail.com>
2020-01-26 15:48:31 +0800

    第一次页面设计

--- src/main/java/View/MainActivity.java
+++ src/main/java/View/MainActivity.java
@@ -4,7 +4,9 @@ import android.app.*;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.agile.R;
@@ -14,5 +16,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        //Button bind
+        ImageButton searchButton = findViewById(R.id.searchButton);
+        ImageButton messageButton = findViewById(R.id.messageButton);
+
+        searchButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        messageButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    public void clickLocation(View v) {
+
     }
 }
